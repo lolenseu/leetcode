53b55d64fc5bc02099629b6214b1bb139ba3da54
lolenseu <lolenseu.koleutejeu248@gmail.com>
2025-05-03 18:02:36 +0800

    a

---
+++ problems/2_Add_Two_Numbers.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        num1 = ""
+        num2 = ""
+        total = ""
+        result = []
+          
+          
+        for i in l1:
+            print(li)
+        return result
+    
+    
+solution = Solution()
+
+l1 = [2,4,3]
+l2 = [5,6,4]
+output = solution.addTwoNumbers(l1, l2)
+
+print("Input: l1 = " + str(l1) + ", l2 = " + str(l2))
+print("Output: " + str(output)) 
